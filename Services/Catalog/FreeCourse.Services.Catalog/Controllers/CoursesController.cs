89d3b4144b08de92c8184b7290f268d8914c3262
samet <74676437+sametyorgunn@users.noreply.github.com>
2023-07-09 00:40:11 +0300

    course controller

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
@@ -0,0 +1,65 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CoursesController : CustomControllerBase
+    {
+        private readonly ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _courseService.GetAllAsync();
+
+            return CreateActionResultInstance(response);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult>GetById(string id)
+        {
+            var response = await _courseService.GetByIdAsync(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [Route("/api[controller]/GetAllByUserId/{Userid}")]
+        public async Task<IActionResult> GetAllByUserId(string Userid)
+        {
+            var response = await _courseService.GetByUserIdAsync(Userid);
+
+            return CreateActionResultInstance(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseCreateDto dto)
+        {
+            var response = await _courseService.CreateAsync(dto);
+
+            return CreateActionResultInstance(response);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update(CourseUpdateDto dto)
+        {
+            var response = await _courseService.UpdateAsync(dto);
+
+            return CreateActionResultInstance(response);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var response = await _courseService.DeleleteAsync(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+
+    }
+}
